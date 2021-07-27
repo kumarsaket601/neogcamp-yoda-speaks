68c27f9d9b95f4b15dcbd447769569f58e890baa
kumarsaket601 <46194436+kumarsaket601@users.noreply.github.com>
2021-07-27 12:54:35 +0530

    javascript handler

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var btnTranslated = document.querySelector('#btn-translated');
+var inputText = document.querySelector('#text-input');
+var outputText = document.querySelector('#output');
 
-console.log("clicked", btnTranslated)
+console.log("clicked",btnTranslated);
+
+function clickHandler ( ){
+
+}
+btnTranslated("clicked",clickHandler)
