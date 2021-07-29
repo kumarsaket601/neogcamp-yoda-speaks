69dd992dbe5e4d05c59ad2f5468f0c1910e917cf
kumarsaket601 <46194436+kumarsaket601@users.noreply.github.com>
2021-07-29 12:40:29 +0530

    all js

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ var inputText = document.querySelector("#text-input");
 var outputText = document.querySelector("#output");
 var log = console.log;
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getServerTranslation(input){
+
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(){
+    log("Something error", error);
+    alert("Something broke Please try it later");
+}
+
+function clickHandler() {
+
+var inputYoda = inputText.value;
 
 
-function clickHandler(){
-
-outputText.innerText = "Welcome!!  " + inputText.value;
-    
+   fetch(getServerTranslation(inputYoda))
+     .then(response => response.json())
+     .then(json => {
+         var converted = json.contents.translated;
+         outputText.innerText = converted;
+    })
+    .catch(errorHandler)
    
 
 };
